21cfe99e927b9cc708c50c70005ec1fc3c83c6ad
Nadia Rasmussen <nadiacrasmussen@gmail.com>
2023-10-11 21:07:59 -0300

    agregue descuento

--- app.js
+++ app.js
@@ -1,64 +1,62 @@
-[];
-const productos = [
-    { nombre: "Adoquin recto", precio: 2500 },
-    { nombre: "Adoquin circular", precio: 2500 },
-    { nombre: "Ekos travertino", precio: 3150 },
-    { nombre: "Ekos zafiro", precio: 3150 },
-    { nombre: "Kebracho", precio: 3150 },
-];
-let carrito = [];
 
+const productos = [
+    { nombre: "Adoquin recto", precio: 2499 },
+    { nombre: "Adoquin circular", precio: 2499 },
+    { nombre: "Ekos travertino", precio: 3149 },
+    { nombre: "Ekos zafiro", precio: 3149 },
+    { nombre: "Kebracho", precio: 3149 },
+];
+
+let carrito = [];
 let seleccion = prompt("Hola! bienvenido a Kamak revestimientos, ¿Desea comprar nuestros productos? si o no");
 
-while (seleccion != "si" && seleccion != "no") {
-    promt("Por favor ingresa si o no");
+while (seleccion !== "si" && seleccion !== "no") {
+    seleccion = prompt("Por favor ingresa si o no");
 }
-if (seleccion == "si") {
+
+if (seleccion === "si") {
     alert("Te mostramos a continuacion nuestra linea de productos");
-    let todoslosProductos = productos.map(
-        (producto) => producto.nombre + " " + producto.precio + "$");
-    alert(todoslosProductos.join("-"))
-} else  if(seleccion == "no") {
+    let todoslosProductos = productos.map((producto) => producto.nombre + " " + "$" + producto.precio);
+    alert(todoslosProductos.join("-"));
+} else if (seleccion === "no") {
     alert("Gracias, esperamos que vuelvas pronto!");
 }
-while (seleccion != "no") {
-    let producto = prompt("agregar producto");
-    let precio = 0;
-    if (producto == "Adoquin recto" || producto == "Adoquin circular" || producto == "Ekos travertino" || producto == "Ekos zafiro" || producto == "Kebracho") {
-        switch (producto) {
-            case "Adoquin recto":
-                precio = 2500;
-                break;
-            case "Adoquin circular":
-                precio = 2500;
-                break;
-            case "Ekos travertino":
-                precio = 3150;
-                break;
-            case "Ekos zafiro":
-                precio = 3150;
-                break;
-            case "Kebracho":
-                precio = 3150;
-                break;
-                default:
-                    break;
 
+while (seleccion !== "no") {
+    let producto = prompt("Agregue los producto");
+    let precio = 0;
+    let encontrado = false;
+
+    for (const item of productos) {
+        if (item.nombre === producto) {
+            precio = item.precio;
+            encontrado = true;
+            break;
         }
+    }
+
+    if (encontrado) {
         let unidades = parseInt(prompt("¿Cuantas unidades desea llevar?"));
         carrito.push({ producto, unidades, precio });
         console.log(carrito);
     } else {
         alert("Ese producto no se encuentra en nuestro stock.");
+        alert("Lista de productos disponibles: " + productos.map(producto => producto.nombre).join(", "));
     }
-    seleccion = prompt("¿Desea agregar mas productos a su compra? Responda si o no");
-    while (seleccion === "no") {
-        alert("Gracias por su compra!");
-        carrito.forEach((carritoFinal) => {
-            console.log(`producto: ${carritoFinal.producto}, unidades:${carritoFinal.unidades},total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`);
-        });
-        break;
-    }
+
+    seleccion = prompt("¿Desea agregar más productos al carrito? si o no");
 }
+
 const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
-console.log(`El total de la compra es: ${total}`);
+const metodoPago = prompt("Seleccione un metodo de pago, recuerde que abonando en efectivo o con transferencia obtiene un 15% de descuento en el total de su compra.");
+
+const montoDescuento = 0.15;
+if (metodoPago === "Efectivo" || metodoPago === "Transferencia") {
+    const montoDescuentoAplicado = total * montoDescuento;
+    const montoDescuentoAplicadoRedondeado = Math.floor(montoDescuentoAplicado);
+    const totalFinal = total - montoDescuentoAplicadoRedondeado;
+
+    alert(`El total de la compra con el 15% de descuento es: ${totalFinal}`);
+} else {
+    alert("No se aplica descuento. El total de la compra es: " + total);
+}
