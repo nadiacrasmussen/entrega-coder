23751a98dad68b4822eaae6952fbe2f931cf6d2d
Nadia Rasmussen <nadiacrasmussen@gmail.com>
2023-11-10 16:06:19 -0300

    agregue fetch

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
 const productos = [
-    { nombre: "Adoquin recto", precio: 2499 },
-    { nombre: "Adoquin circular", precio: 2499 },
-    { nombre: "Ekos travertino", precio: 3149 },
-    { nombre: "Ekos zafiro", precio: 3149 },
-    { nombre: "Kebracho", precio: 3149 },
+    { nombre: "Kubik onix", precio: 5640, Uso: "Exterior/Interior", imagen: "../img/productos/kubik-onix.png" },
+    { nombre: "Adoquin circular", precio: 5640, Uso: "Exterior/Interior", imagen: "../img/productos/adoquin-circular.png" },
+    { nombre: "Ladrillo visto", precio: 5640, Uso: "Exterior/Interior", imagen: "../img/productos/ladrillo-visto.png" },
+    { nombre: "Umha onix", precio: 5640, Uso: "Exterior/Interior", imagen: "../img/productos/umha-onix.png" },
+    { nombre: "Kebracho", precio: 5640, Uso: "Exterior/Interior", imagen: "../img/productos/kebracho.png" },
 ];
+let productosFilter = productos;
 
 let carrito = [];
-
 function mostrarProductos() {
     let productosDom = document.getElementById("productos");
-    todoslosProductos = productos.map((producto) => producto.nombre + " " + "$" + producto.precio);
-    productosDom.innerHTML = `productos: ${todoslosProductos.join("-")}`;
+
+    productosDom.innerHTML =""
+    for (let index = 0; index < productosFilter.length; index++) {
+        const producto = productosFilter[index];
+        productosDom.innerHTML += `
+        <div class="card" style="width: 24rem;">
+            <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+            <div class="card-body">
+                <h5 class="card-title">${producto.nombre}</h5>
+                <p class="card-text">
+                <ul>
+                    <li>Cantidad: 8 Bultos por M2</li>
+                    <li>Uso: ${producto.Uso}</li>
+                    <li>Precio $ ${producto.precio}</li>
+                </ul>
+                </p>
+                <a href="#" onclick="enviarProducto(event,'${producto.nombre}')" class="btn btn-primary solicitud">Solicitá tu presupuesto</a>
+            </div>
+        </div>
+    `;
+    }
 }
 
-const formulario = document.getElementById("formProductos");
-formulario.addEventListener("submit", enviarProducto);
+const formulario = document.getElementById("formBuscar");
+formulario.addEventListener("submit", buscar);
+function buscar() {
+   
+    const busqueda = document.getElementById("buscar").value;
 
-function enviarProducto(e) {
-    let nombreProducto = document.getElementById("productoInput").value;
+    productosFilter=productos.filter((producto )=>{
+        return producto.nombre.toLocaleLowerCase() == busqueda.toLocaleLowerCase()
+    })
+    mostrarProductos();
+} 
+function enviarProducto(e, nombreProducto) {
     e.preventDefault();
     let encontrado;
     for (const item of productos) {
         if (item.nombre === nombreProducto) {
             carrito.push(item);
-            encontrado = true
+            encontrado = true;
             break;
         }
     }
@@ -35,87 +61,50 @@ function enviarProducto(e) {
 
     let carritoDom = document.getElementById("carritoDom");
 
-    if(carrito){
+    if (carrito) {
         carritoDom.innerHTML = carrito.map((producto) => producto.nombre + " " + "$" + producto.precio);
     }
-
 
     console.log(nombreProducto);
 }
 
-
-function completarVenta(e){
+function completarVenta(e) {
     e.preventDefault();
-    console.log(carrito)
+    console.log(carrito);
 
-    const total = carrito.reduce((acc, el) => acc + el.precio , 0);
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
 
-    console.log(total)
-    localStorage.setItem("carrito",carrito)
+    console.log(total);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
+function mostrarDolares() {
+    const dolaresDiv = document.getElementById("dolares");
+
+    fetch("https://dolarapi.com/v1/dolares")
+        .then((respuesta) => {
+            console.log("el servidor respondio");
+
+            const promesa = respuesta.json();
+            promesa
+                .then((dolares) => {
+                    console.log(dolares);
+
+                    for (let index = 0; index < dolares.length; index++) {
+                        const dolar = dolares[index];
+                        dolaresDiv.innerHTML += `
+                        <div class="dolares">
+                        <ul>
+                        <li class="usd">${dolar.moneda} ${dolar.nombre} Compra: ${dolar.compra} Venta: ${dolar.venta} <li>
+                        <ul>
+                        </div>
+                         `;
+                    }
+                })
+                .catch(() => {});
+        })
+        .catch((error) => {});
+}
+mostrarDolares();
 mostrarProductos();
-/* 
-const productos = [
-    { nombre: "Adoquin recto", precio: 2499 },
-    { nombre: "Adoquin circular", precio: 2499 },
-    { nombre: "Ekos travertino", precio: 3149 },
-    { nombre: "Ekos zafiro", precio: 3149 },
-    { nombre: "Kebracho", precio: 3149 },
-];
 
-let carrito = [];
-let seleccion = prompt("Hola! bienvenido a Kamak revestimientos, ¿Desea comprar nuestros productos? si o no");
-
-while (seleccion !== "si" && seleccion !== "no") {
-    seleccion = prompt("Por favor ingresa si o no");
-}
-
-if (seleccion === "si") {
-    alert("Te mostramos a continuacion nuestra linea de productos");
-    let todoslosProductos = productos.map((producto) => producto.nombre + " " + "$" + producto.precio);
-    alert(todoslosProductos.join("-"));
-} else if (seleccion === "no") {
-    alert("Gracias, esperamos que vuelvas pronto!");
-}
-
-while (seleccion !== "no") {
-    let producto = prompt("Agregue los producto");
-    let precio = 0;
-    let encontrado = false;
-
-    for (const item of productos) {
-        if (item.nombre === producto) {
-            precio = item.precio;
-            encontrado = true;
-            break;
-        }
-    }
-
-    if (encontrado) {
-        let unidades = parseInt(prompt("¿Cuantas unidades desea llevar?"));
-        carrito.push({ producto, unidades, precio });
-        console.log(carrito);
-    } else {
-        alert("Ese producto no se encuentra en nuestro stock.");
-        alert("Lista de productos disponibles: " + productos.map(producto => producto.nombre).join(", "));
-    }
-
-    seleccion = prompt("¿Desea agregar más productos al carrito? si o no");
-}
-
-const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
-const metodoPago = prompt("Seleccione un metodo de pago, recuerde que abonando en efectivo o con transferencia obtiene un 15% de descuento en el total de su compra.");
-
-const montoDescuento = 0.15;
-if (metodoPago === "Efectivo" || metodoPago === "Transferencia") {
-    const montoDescuentoAplicado = total * montoDescuento;
-    const montoDescuentoAplicadoRedondeado = Math.floor(montoDescuentoAplicado);
-    const totalFinal = total - montoDescuentoAplicadoRedondeado;
-
-    alert(`El total de la compra con el 15% de descuento es: ${totalFinal}`);
-} else {
-    alert("No se aplica descuento. El total de la compra es: " + total);
-}
-
- */
